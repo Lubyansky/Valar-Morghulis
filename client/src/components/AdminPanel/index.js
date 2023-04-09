eb2848ee0f882846b9f50e273ed0b07e2f9120e5
Lubyansky <noobsa87@gmail.com>
2023-04-09 22:54:35 +0500

    Revert "Пуньк"
    This reverts commit b46c4ddcb06e015fe7e1bdb06bef072877163386.

--- client/src/components/AdminPanel/index.js
+++
@@ -1,8 +0,0 @@
-import AdminTable from "@/components/AdminPanel/AdminTable"
-import ChangeItem from "@/components/AdminPanel/ChangeItem"
-import TableSearch from "@/components/AdminPanel/Search"
-
-
-export default [
-    AdminTable, ChangeItem, TableSearch
-]
--- client/src/components/UI/index.js
+++
@@ -1,8 +0,0 @@
-import Pagination from "@/components/UI/Pagination"
-import Modal from "@/components/UI/Modal"
-import CustomButton from "@/components/UI/CustomButton"
-
-
-export default [
-    Pagination, Modal, CustomButton
-]
